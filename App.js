fcbb66af1efb7c7a7717a74a7d06974006af774e
Mikepro <miguelalexander890@gmail.com>
2023-08-10 15:14:11 -0600

    Primer Avanze

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View , SafeAreaView, Image} from 'react-native';
+import ViewInicial from './source/Components/ViewInicial';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.image}
+      source= {require('./assets/boyContainer.png')}/>
+      <ViewInicial title={'Hola Mikepro'}/>
     </View>
   );
 }
@@ -13,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9775FA',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width: '100%',
+    height:'90%',
+    marginTop:100,
+    marginLeft:70,
+    position:'absolute'
+  },
+  
+ 
 });
